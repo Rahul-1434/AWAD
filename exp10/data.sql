0633e955386c81f88e1b2b35775154c0f32fe9d7
Rahul-1434 <157436820+Rahul-1434@users.noreply.github.com>
2024-10-24 19:31:41 -0700

    Add files via upload

---
+++ exp10/data.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 25, 2024 at 04:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `exp10`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+CREATE TABLE `data` (
+  `Name` text NOT NULL,
+  `Age` smallint(3) NOT NULL,
+  `Gender` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `data`
+--
+
+INSERT INTO `data` (`Name`, `Age`, `Gender`) VALUES
+('Rahul', 20, 'Male'),
+('Kishore', 20, 'Male'),
+('varun', 20, 'Male'),
+('govindu', 20, 'Male'),
+('Praveen', 20, 'Male');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ exp10/dbconn.php
@@ -0,0 +1,5 @@
+<?php
+    $con = new mysqli('localhost', 'root', '', 'exp10');
+    if ($con->connect_error)
+    die("Connection Failure !");
+?>
