5e3577a872b0dfb226e8e347847d73a84b8933fc
Rahul-1434 <157436820+Rahul-1434@users.noreply.github.com>
2024-11-06 20:52:09 +0530

    Add files via upload

---
+++ exp9/welcome.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Dashboard</title>
+    </head>
+    <body>
+        <div style='text-align:right;'>
+            <a href='signin.htm'>Signin</a>
+        </div>
+        <h3>Registered successfully!</h3>
+    </body>
+</html>
